8ef089d8ab1b0045cfa36d2c2ccf8848a20e72a4
JeonCM <wsd3576@gmail.com>
2025-05-07 14:31:14 +0900

    Update PlayerController.cs

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -6,7 +6,7 @@ using static UnityEngine.GraphicsBuffer;
 
 public class PlayerController : BaseController
 {
-    private Camera camera;
+    private new Camera camera;
 
     private void Start()
     {
